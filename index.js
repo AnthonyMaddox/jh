37e8a1df0ef8bde384052a6aff81fa2e2a0a0873
AnthonyMaddox <anthonyjmaddox@gmail.com>
2021-03-24 14:37:22 -0400

    contact form go

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ contactEmail.verify((error) => {
     console.log("Ready to Send");
   }
 });
-router.post("/api/contact", (req, res) => {
+router.post("/api/passwords", (req, res) => {
   const name = req.body.name;
   const email = req.body.email;
   const message = req.body.message;
@@ -49,25 +49,25 @@ router.post("/api/contact", (req, res) => {
   });
 });
 // Put all API endpoints under '/api'
-app.get("/api/passwords", (req, res) => {
-  const count = 5;
+// app.get("/api/passwords", (req, res) => {
+//   const count = 5;
 
-  // Generate some passwords
-  const passwords = Array.from(Array(count).keys()).map((i) =>
-    generatePassword(12, false)
-  );
+//   // Generate some passwords
+//   const passwords = Array.from(Array(count).keys()).map((i) =>
+//     generatePassword(12, false)
+//   );
 
-  // Return them as json
-  res.json(passwords);
+//   // Return them as json
+//   res.json(passwords);
 
-  console.log(`Sent ${count} passwords`);
-});
+//   console.log(`Sent ${count} passwords`);
+// });
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back React's index.html file.
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/jhome/build/index.html"));
-});
+// // The "catchall" handler: for any request that doesn't
+// // match one above, send back React's index.html file.
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname + "/jhome/build/index.html"));
+// });
 
 const port = process.env.PORT || 5000;
 app.listen(port);
